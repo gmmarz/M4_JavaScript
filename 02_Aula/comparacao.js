840bddee435bf7d12757ad2593eaa707f09f8d37
Guilherme <guilhermemarzano@gmail.com>
2024-05-20 21:57:39 -0300

    end of class

---
+++ 02_Aula/comparacao.js
@@ -0,0 +1,47 @@
+// // Primeiro caso - Comparação
+// console.log('Primeiro caso')
+// console.log(5 == 5) //true
+// console.log('5' == 5) //true
+// //Dois iguais compara se o valor é igual
+
+// //Segundo caso comparação
+// console.log('Segundo caso')
+// console.log(5 === 5) //true
+// console.log('5' === 5) //false
+// //compara se o valor e o tipo são iguais
+
+
+// //Operadores comparação
+// // >, >= , < , <= 
+
+// console.log(5 <2) //true
+// console.log(3<=3) //true
+
+// //Operadores lógicos 
+// //Operador and &&
+// // asci pipe  alt + 124
+// console.log(false && false)//false
+// console.log(false && true)//false
+// console.log(true && false)//false
+// console.log(true && true) //true
+
+// //Operadores lógicos 
+// //Operador or ||
+// // asci pipe  alt + 124
+// console.log(false || false)//false
+// console.log(false || true)//true
+// console.log(true || false)//true
+// console.log(true || true) //true
+
+// //Operador NOT ,!
+// let nome = ''
+// //Pois em js string vazia, 0  são consideradas variáveis falsianas.
+// console.log(!nome) // True
+
+//IF em js
+if(!nome){
+     console.log('Fi c esqueceu de colocá o nome')
+}
+else{
+     console.log('Tudo certin')
+}
