def29c309357771a26440caaa8c9acfb0959c9d2
Guilherme Marzano <guilhermemarzano@gmail.com>
2024-08-27 09:03:22 -0300

    update data

--- 12_Aula_objJson/script.js
+++ 12_Aula_objJson/script.js
@@ -1,119 +1,121 @@
-
 // Componentes (Elementos HTML)
 function AcoesTarefa(tarefa) {
-    // Criando Div
-    let divAcoes = document.createElement('div')
-    
-    // Criando Checkbox
-    const checkboxTarefa = document.createElement("input");
-    checkboxTarefa.type = "checkbox";
-    checkboxTarefa.checked = tarefa.completa;
+  // Criando Div
+  let divAcoes = document.createElement('div')
+  
+  // Criando Checkbox
+  const checkboxTarefa = document.createElement("input");
+  checkboxTarefa.type = "checkbox";
+  checkboxTarefa.checked = tarefa.completa;
 
-    checkboxTarefa.addEventListener(
-        "click", () => switchFinalizarTarefa(tarefa.id))
+  checkboxTarefa.addEventListener(
+      "click", () => switchFinalizarTarefa(tarefa.id))
 
-    // Criando botão de editar
-    const botaoEditar = document.createElement('button')
+  // Criando botão de editar
+  const botaoEditar = document.createElement('button')
 
-    const iconeEditar = document.createElement('ion-icon')
-    iconeEditar.name = 'create-outline'
-    botaoEditar.appendChild(iconeEditar)
-    
-    botaoEditar.addEventListener(
-        "click", () => editarTarefa(tarefa.id))
+  const iconeEditar = document.createElement('ion-icon')
+  iconeEditar.name = 'create-outline'
+  botaoEditar.appendChild(iconeEditar)
+  
+  botaoEditar.addEventListener(
+      "click", () => editarTarefa(tarefa.id))
 
-    // Criando botão de excluir
-    const botaoExcluir = document.createElement('button')
+  // Criando botão de excluir
+  const botaoExcluir = document.createElement('button')
 
-    const iconeExcluir = document.createElement('ion-icon')
-    iconeExcluir.name = 'trash-bin-outline'
-    botaoExcluir.appendChild(iconeExcluir)
-    
-    botaoExcluir.addEventListener(
-        "click", () => excluirTarefa(tarefa.id))
+  const iconeExcluir = document.createElement('ion-icon')
+  iconeExcluir.name = 'trash-bin-outline'
+  botaoExcluir.appendChild(iconeExcluir)
+  
+  botaoExcluir.addEventListener(
+      "click", () => excluirTarefa(tarefa.id))
 
-    divAcoes.appendChild(checkboxTarefa)
-    divAcoes.appendChild(botaoEditar)
-    divAcoes.appendChild(botaoExcluir)
-    divAcoes.classList.add('acoes')
+  divAcoes.appendChild(checkboxTarefa)
+  divAcoes.appendChild(botaoEditar)
+  divAcoes.appendChild(botaoExcluir)
+  divAcoes.classList.add('acoes')
 
-    return divAcoes
+  return divAcoes
 }
 
 function Tarefa(tarefa) {
-  // Construindo LI
-  const liTarefa = document.createElement("li");
-  // liTarefa.textContent = `${tarefa} - ${descricao} -> `
+// Construindo LI
+const liTarefa = document.createElement("li");
+// liTarefa.textContent = `${tarefa} - ${descricao} -> `
 
-  const h2Tarefa = document.createElement("h2");
-  h2Tarefa.textContent = tarefa.nome;
+const h2Tarefa = document.createElement("h2");
+h2Tarefa.textContent = tarefa.nome;
 
-  const pDescricao = document.createElement("p");
-  pDescricao.textContent = tarefa.descricao;
+const pDescricao = document.createElement("p");
+pDescricao.textContent = tarefa.descricao;
 
-  const divInfo = document.createElement("div");
-  divInfo.appendChild(h2Tarefa);
-  divInfo.appendChild(pDescricao);
+const divInfo = document.createElement("div");
+divInfo.appendChild(h2Tarefa);
+divInfo.appendChild(pDescricao);
 
-  const divAcoes = AcoesTarefa(tarefa)
+const divAcoes = AcoesTarefa(tarefa)
 
-  liTarefa.appendChild(divInfo);
-  liTarefa.appendChild(divAcoes);
-  liTarefa.classList.add("tarefa");
-  
-  if (tarefa.completa) {
-    liTarefa.classList.add("concluida");
-  } else {
-    liTarefa.classList.remove("concluida");
-  }
+liTarefa.appendChild(divInfo);
+liTarefa.appendChild(divAcoes);
+liTarefa.classList.add("tarefa");
 
-  return liTarefa;
+if (tarefa.completa) {
+  liTarefa.classList.add("concluida");
+} else {
+  liTarefa.classList.remove("concluida");
+}
+
+return liTarefa;
 }
 
 // -----------------------------------
 // Utilitarios
 function criarId(tarefas) {
-    let maiorId;
-    
-    for (let tarefa of tarefas) {
-        if (maiorId == undefined || tarefa.id > maiorId) {
-            maiorId = tarefa.id
-        }
-    }
+  let maiorId;
+  
+  for (let tarefa of tarefas) {
+      if (maiorId == undefined || tarefa.id > maiorId) {
+          maiorId = tarefa.id
+      }
+  }
 
-    return maiorId + 1
+  if (maiorId == undefined){
+    return 1
+  }  
+  return maiorId + 1
 }
 
 // Lógica da Tela
 let tarefasState = [
-  {
-    id: 1,
-    nome: "Estudar AWS",
-    descricao: "Realizar curso do andré iacono",
-    completa: true,
-  },
-  {
-    id: 2,
-    nome: "Nginx para Deploy",
-    descricao: "Na marra",
-    completa: false,
-  },
+{
+  id: 1,
+  nome: "Estudar AWS",
+  descricao: "Realizar curso do andré iacono",
+  completa: true,
+},
+{
+  id: 2,
+  nome: "Nginx para Deploy",
+  descricao: "Na marra",
+  completa: false,
+},
 ];
 
 // Listar Tarefas
 function listarTarefas(tarefas) {
-  const ulTarefas = document.getElementById("tarefas");
-  ulTarefas.textContent = "";
+const ulTarefas = document.getElementById("tarefas");
+ulTarefas.textContent = "";
 
-  if (tarefas.length == 0) {
-    ulTarefas.textContent = "Não foram encontrados resultados...";
-    return;
-  }
+if (tarefas.length == 0) {
+  ulTarefas.textContent = "Não foram encontrados resultados...";
+  return;
+}
 
-  tarefas.map((tarefa) => {
-    const liTarefa = Tarefa(tarefa);
-    ulTarefas.appendChild(liTarefa);
-  });
+tarefas.map((tarefa) => {
+  const liTarefa = Tarefa(tarefa);
+  ulTarefas.appendChild(liTarefa);
+});
 
 //   for (let tarefa of tarefas) {
 //     const liTarefa = Tarefa(tarefa);
@@ -122,8 +124,8 @@ function listarTarefas(tarefas) {
 }
 
 function handleListarTarefas() {
-  // Logica para buscar as tarefas de outro lugar.
-  listarTarefas(tarefasState);
+// Logica para buscar as tarefas de outro lugar.
+listarTarefas(tarefasState);
 }
 
 window.addEventListener("load", handleListarTarefas);
@@ -132,13 +134,13 @@ window.addEventListener("load", handleListarTarefas);
 const inputPesquisar = document.getElementById("pesquisar");
 
 function handlePesquisarTarefas() {
-  let pesquisa = inputPesquisar.value.toLowerCase();
+let pesquisa = inputPesquisar.value.toLowerCase();
 
-  let resultado = tarefasState.filter((tarefa) =>
-    tarefa.nome.toLowerCase().includes(pesquisa)
-  );
+let resultado = tarefasState.filter((tarefa) =>
+  tarefa.nome.toLowerCase().includes(pesquisa)
+);
 
-  listarTarefas(resultado);
+listarTarefas(resultado);
 }
 
 inputPesquisar.addEventListener("input", handlePesquisarTarefas);
@@ -147,52 +149,52 @@ inputPesquisar.addEventListener("input", handlePesquisarTarefas);
 const formulario = document.getElementById("form-tarefa");
 
 function handleAdicionarTarefa(event) {
-  event.preventDefault();
+event.preventDefault();
 
-  // Capturando do Formulaŕio
-  let novaTarefa = {
-    id: criarId(tarefasState),
-    nome: formulario.tarefa.value,
-    descricao: formulario.descricao.value,
-    completa: false,
-  };
+// Capturando do Formulaŕio
+let novaTarefa = {
+  id: criarId(tarefasState),
+  nome: formulario.tarefa.value,
+  descricao: formulario.descricao.value,
+  completa: false,
+};
 
-  tarefasState.push(novaTarefa);
-  listarTarefas(tarefasState);
+tarefasState.push(novaTarefa);
+listarTarefas(tarefasState);
 
-  formulario.reset()
+formulario.reset()
 }
 
 formulario.addEventListener("submit", handleAdicionarTarefa);
 
 // Excluir tarefa
 function excluirTarefa(tarefaId) {
-    let confirmacao = confirm('Deseja realmente excluir a tarefa?')
+  let confirmacao = confirm('Deseja realmente excluir a tarefa?')
 
-    if (confirmacao) {
-        tarefasState = tarefasState
-        .filter((tarefa) => tarefa.id != tarefaId)
+  if (confirmacao) {
+      tarefasState = tarefasState
+      .filter((tarefa) => tarefa.id != tarefaId)
 
-        listarTarefas(tarefasState)    
-    }
+      listarTarefas(tarefasState)    
+  }
 }
 
 // Editar tarefa
 function editarTarefa(tarefaId) {
-    let tarefa = tarefasState
-        .find((tarefa) => tarefa.id == tarefaId)
+  let tarefa = tarefasState
+      .find((tarefa) => tarefa.id == tarefaId)
 
-    tarefa.nome = prompt('Digite o novo nome da tarefa: ')
-    tarefa.descricao = prompt('Digite a nova descrição da tarefa: ')
+  tarefa.nome = prompt('Digite o novo nome da tarefa: ')
+  tarefa.descricao = prompt('Digite a nova descrição da tarefa: ')
 
-    listarTarefas(tarefasState)
+  listarTarefas(tarefasState)
 }
 
 // Switch Finalizar Tarefa
 function switchFinalizarTarefa(tarefaId) {
-    let tarefa = tarefasState
-        .find((tarefa) => tarefa.id == tarefaId)
+  let tarefa = tarefasState
+      .find((tarefa) => tarefa.id == tarefaId)
 
-    tarefa.completa = !tarefa.completa
-    listarTarefas(tarefasState)
+  tarefa.completa = !tarefa.completa
+  listarTarefas(tarefasState)
 }
