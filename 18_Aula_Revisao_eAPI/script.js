be6a4b3e07dd064c27e4b81460891ca9d57cc93d
guilherme <guilhermemarzano@gmail.com>
2024-09-16 21:40:14 -0300

    Fim da aula

--- 18_Aula_Revisao_eAPI/script.js
+++ 18_Aula_Revisao_eAPI/script.js
@@ -2,6 +2,23 @@ const API_URL = 'https://api.themoviedb.org/3'
 const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYTljOWU5ZmM2NjUzNDVjODAzMmY1NGRiN2Q1NzBjYyIsIm5iZiI6MTcyNjUyNTAwMC4wOTY0ODgsInN1YiI6IjY2ZThhZGFhYmI4MzYxYTFkNmE5ZGQ2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.6wM_TZGOqXjWAWAPzS-YTLtyWt7OVRb44CGo1wNeS_4'
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w500'
 
+
+function Filme(filme) {
+    const parser = new DOMParser()
+
+    const doc = parser.parseFromString(`
+        <div class="filme">
+            <img src="${IMAGE_URL}${filme.poster_path}" alt="" />
+            <div>
+                <h3>${filme.title}</h3>
+                <p>${filme.overview}</p>
+            </div>
+        </div>
+        `, 'text/html')
+
+    return doc.body.firstChild
+}
+
 async function buscarFilmesPeloFiltro(filtro = {}) {
     try {
         let requestUrl = `${API_URL}/discover/movie?language=pt-BR`;
@@ -25,17 +42,39 @@ async function buscarFilmesPeloFiltro(filtro = {}) {
         })
 
         const data = await response.json()
-        console.log(data)
-        return data
+        return data.results
     } catch (error) {
         console.error(error)
-        return {
-            results: []
-        }
+        return []
     }
 }
 
-const filter = {
-    sortBy: "release_date.desc",
-    maxDate: new Date().toISOString()
+
+async function handleListarFilmesDiscover() {
+    const filtro = {
+        sortBy: "release_date.desc",
+        maxDate: new Date().toISOString()
+    }
+
+    const filmes = await buscarFilmesPeloFiltro(filtro)
+
+    const discoverSection = document.getElementById('discover')
+    discoverSection.innerText = ''
+    for (let filme of filmes) {
+        discoverSection.appendChild(Filme(filme))
+    }
+}
+
+async function handleListarFilmesDeAcao() {
+    // 1. Construir o Filtro, passando o campo "genre"
+
+    // 2. Façam uma requisição no thunder client, para descobrir qual é o identificador dos filmes de ação:
+    // https://api.themoviedb.org/3/genre/movie/list?language=pt
+}
+
+async function handleListarFilmesDeComedia() {
+    // 1. Construir o Filtro, passando o campo "genre"
+
+    // 2. Façam uma requisição no thunder client, para descobrir qual é o identificador dos filmes de ação:
+    // https://api.themoviedb.org/3/genre/movie/list?language=pt
 }
